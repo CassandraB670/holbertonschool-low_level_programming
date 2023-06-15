749494d14ed8945be52aa01eea652db2de9e8ccc
CassandraB670 <cassandra.boudier@gmail.com>
2023-06-15 14:42:45 -0700

    Change the C-programm of the file 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-/**
- * main : C program that prints the size on various types
- *
- * return : Always 0 (success)
- */
 
+/**
+* main - Entry point of the program
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("Size of char: %d byte(s)\n", sizeof(char));
-	printf("Size of int: %d byte(s)\n", sizeof(int));
-	printf("Size of long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of float: %d byte(s)\n", sizeof(float));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
