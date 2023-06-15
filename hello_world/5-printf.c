05097d920d33da5c0f30581fb35ca5ed94d46a71
CassandraB670 <cassandra.boudier@gmail.com>
2023-06-15 13:59:50 -0700

    Change the script for file 5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
 /**
-* main : Entry for the program Hello, printf.
-*
-* return : Always 0 (success)
-*/
+ * main - Print a string with printf
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
