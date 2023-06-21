8da467cee217293cb679a410bd93d6037fcb549f
CassandraB670 <cassandra.boudier@gmail.com>
2023-06-21 04:21:06 -0700

    modify the file 7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -13,10 +13,9 @@ int print_last_digit(int n)
 	int last_d = n % 10;
 
 	if (last_d < 0)
-	{
-		last_d = last_d * (-1);
-		return (last_d);
-	}
-	else
-		return (last_d);
+		last_d *= -1;
+
+	_putchar(last_d + '0');
+
+	return (last_d);
 }
