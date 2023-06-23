aa350b88fceb8717a61263c638f91c379bbc9559
CassandraB670 <cassandra.boudier@gmail.com>
2023-06-23 01:20:56 -0700

    Complete the README.md, and the file named main.h and create a file named 6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line in the terminal
+ *
+ * @n: variable
+ */
+
+void print_line(int n)
+{
+	int number;
+
+	if (n > 0)
+	{
+		number = 0;
+		
+		while (number < n)
+		{
+			_putchar('_');
+			number++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
