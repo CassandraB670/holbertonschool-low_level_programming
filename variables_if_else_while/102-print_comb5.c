ac6e6a2c203e9942b9f23df5859bcf672d24b24b
CassandraB670 <cassandra.boudier@gmail.com>
2023-06-23 06:19:36 -0700

    Complete the README.md and create a file named 102-print_comb5.c

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int number1;
+	int number2;
+
+	for (number1 = 0; number1 < 100; number1++)
+	{
+		for (number2 = number1 + 1; number2 < 100; number2++)
+		{
+			putchar((number1) / 10 + '0');
+			putchar((number1) % 10 + '0');
+			putchar(' ');
+			putchar((number2) / 10 + '0');
+			putchar((number2) % 10 + '0');
+
+			if (number1 != 98 || number2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
