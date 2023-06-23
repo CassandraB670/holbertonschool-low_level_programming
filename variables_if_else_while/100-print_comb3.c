2420be91beeb690dc3c94b6a051edd475c839cce
CassandraB670 <cassandra.boudier@gmail.com>
2023-06-23 05:42:21 -0700

    Modify the file named 100-print_comb3.c

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 /**
- * 
+ * main - prints all possible different combinations
+ *
+ * Return: always 0 (success)
  */
 
 int main(void)
@@ -11,7 +13,7 @@ int main(void)
 
 	for (number1 = 0; number1 <= 9; number1++)
 	{
-		for(number2 = number1 + 1; number2 < 10; number2++)
+		for (number2 = number1 + 1; number2 < 10; number2++)
 		{
 			putchar(number1 + '0');
 			putchar(number2 + '0');
