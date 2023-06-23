d0893a242384fc9ab15d1fc6450e912e31af901c
CassandraB670 <cassandra.boudier@gmail.com>
2023-06-23 03:56:05 -0700

    Complete the README.md and create a file named 9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 1; number <= 100; number++)
+	{
+		if (number % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (number % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", number);
+		}
+	}
+	printf("\n");
+	return (0);
+}
+
+
+
