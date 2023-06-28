5e7db1e808efd0ec61d456ad78d9cfe529fa34e1
CassandraB670 <cassandra.boudier@gmail.com>
2023-06-28 01:17:59 -0700

    Modify the main.h, the README.md and add file 100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - convert a string to an integer
+ *
+ * @s: pointer
+ *
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	unsigned int result = 0;
+	int sign = 1;
+	int i = 0;
+
+	while (s[i] && !(s[i] >= '0' && s[i] <= '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+	return (sign * result);
+}
