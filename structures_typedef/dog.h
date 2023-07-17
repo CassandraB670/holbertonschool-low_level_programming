e9cb6de744b922f91817f3ff1314a94ca1f60853
CassandraB670 <cassandra.boudier@gmail.com>
2023-07-17 13:31:09 -0700

    create and complete README.md and dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new structure for the dog
+ *
+ * @name: member for define the dog's name
+ *
+ * @age: member for define the dog's age
+ *
+ * @owner: member for define the dog's owner
+ *
+ * Definition: this structure define name, age and owner for a dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
