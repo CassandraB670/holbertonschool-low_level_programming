1b94e72b224d5bce3bdb11340108efaafef2d521
CassandraB670 <cassandra.boudier@gmail.com>
2023-07-20 15:00:00 +0000

    test push from docker

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_all -
+ *
+ *
+ *
+ *
+*/
+
+void print_all(const char * const format, ...)
+{
+
+}
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_all("ceis", 'B', 3, "stSchool");
+    return (0);
+}
