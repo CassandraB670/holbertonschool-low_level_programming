5513dc80d34c63b22238b1918e23acd960796e1b
CassandraB670 <cassandra.boudier@gmail.com>
2023-07-21 09:46:16 +0200

    change description

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,52 +1,51 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
+#include "main.h"
 
 /**
- * print_all - function that prints anything
- *
- * @format: list of types of arguments passed to the function
- *
-*/
-
+ * print_all - Prints anything based on the format provided.
+ * @format: The format string that specifies the types of arguments.
+ * Return: void
+ */
 void print_all(const char * const format, ...)
 {
-    va_list ap;
-    unsigned int i = 0;
-    char *str;
-    char current_char;
+	va_list args;
+	unsigned int i = 0;
+	char *str;
+	char current_char;
 
-    va_start(ap, format);
+	va_start(args, format);
 
-    while (format && format[i])
-    {
-        current_char = format[i];
+	while (format && format[i])
+	{
+		current_char = format[i];
 
-        switch (current_char)
-        {
-            case 'c':
-                printf("%c", va_arg(ap, int));
-                break;
-            case 'i':
-                printf("%d", va_arg(ap, int));
-                break;
-            case 'f':
-                printf("%f", va_arg(ap, double));
-                break;
-            case 's':
-                str = va_arg(ap, char *);
-                if (str == NULL)
-                    str = "(nil)";
-                printf("%s", str);
-                break;
-            default:
-                i++;
-                continue;
-        }
-        if (format[i + 1] != '\0')
-            printf(", ");
+		switch (current_char)
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+			default:
+				i++;
+				continue;
+		}
 
-        i++;
-    }
-    printf("\n");
-    va_end(ap);
+		if (format[i + 1] != '\0')
+			printf(", ");
+
+		i++;
+	}
+	printf("\n");
+	va_end(args);
 }
