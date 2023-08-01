67b1db5ec52475bc0d9eb86b492e4fcb4edc6514
CassandraB670 <cassandra.boudier@gmail.com>
2023-08-01 12:35:04 -0700

    change the code for return malloc

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,12 +8,18 @@
  *
  * @str: string we want to add
  *
- * Return: new address
+ * Return: NULL or new address
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
+
+	if (str == NULL)
+		return (NULL);
+
+	if (new_node == NULL)
+		return (NULL);
 
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
